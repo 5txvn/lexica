2d2530dacc3f5444cefe87990ee9a3606e6278f7
Steven Livingston <therealenny1@gmail.com>
2023-12-24 21:55:43 -0600

    add dotenv and sessions

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+//express and bodyparser setup
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+
+//use dotenv
+require('dotenv').config();
+
+//set up sessions usage
+const session = require('express-session');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
 //set up the signup page
 app.get('/sign-up', (req, res) => {
     res.sendFile(__dirname + "/signup.html")
+})
+
+app.post('/sign-up', (req, res) => {
+
 })
