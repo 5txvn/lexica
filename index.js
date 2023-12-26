96dcc184dcd740a2354b84ae05fc1060743e9da5
Steven Livingston <therealenny1@gmail.com>
2023-12-25 18:17:44 -0600

    add ejs to index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
 
 //use dotenv
 require('dotenv').config();
